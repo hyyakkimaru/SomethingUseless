3020e1828be915b77235b2268d356ad6e2c48694
Hyakkimaruu <53900014+Hyakkimaruu@users.noreply.github.com>
2019-08-08 17:52:54 +0700

    Add files via upload

---
+++ Form Login2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+              <title>Sebuah Tugas</title>
+              <style type="text/css">
+                body  {
+                  background-image: url(555570_screenshots_20190324033438_1.jpg) ;
+                  
+                div.box 
+              	   background-color: cyan;
+              	   background-image: url(555570_screenshots_20190324033438_1.jpg) ;
+                   width:320px;
+                   margin:70px auto;
+                   padding:40;
+                   box-shadow: 10px 10px 10px;
+                   }   
+                       
+              </style>        
+    </head>
+<body>       
+              <div class="box">
+              <form>
+              
+              <br>
+                
+              <br>
+              
+              <center><h1><i>SUPRISE MOTHERFUCKER</i></h1></center>
+                                                
+              
+              <pre><p><font size=3> <center> <input type="text" placeholder="username"> 
+              <pre> <center> <input type="password"placeholder="password"> </p>
+              <button>      Ok      </button>
+              <style>
+                    body {color:white;}
+              </style>
+              </form>
+              </div>
+</body>
+</html>
